8c30f9647dc412ae0dc73d99e428f41bf583d82d
Sabbir1441 <sabbirahmed1441@gmail.com>
2025-01-20 02:04:11 +0600

    done the details page work

--- src/Pages/Home/TopScholarship/TopScholarship.jsx
+++ src/Pages/Home/TopScholarship/TopScholarship.jsx
@@ -1,21 +1,12 @@
-import { useEffect, useState } from "react";
-import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 
 const TopScholarship = () => {
 
-    const [topScholarship , setTopScholarship] = useState([]);
-    useEffect(() => {
-        fetch('Scholarship.json')
-        .then(res => res.json())
-        .then(data => setTopScholarship(data))
-    })
+    
 
     return (
-        <section>
-            <SectionTitle
-            heading={'Top scholarship'}
-            ></SectionTitle>
-        </section>
+    <div>
+
+    </div>
     );
 
 };
